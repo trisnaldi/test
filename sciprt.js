51070ffb2e53cb5446c8ad08b83eeaa3d52be9e1
Trisnaldi elangga <150431149+trisnaldi@users.noreply.github.com>
2023-11-11 09:20:53 +0700

    Add files via upload

---
+++ sciprt.js
@@ -0,0 +1,15 @@
+function Login() {
+    var done = 0;
+    var username = document.login.username.value;
+    var password = document.login.password.value;
+    if (username == "Mabim" && password =="Mabim"){
+         if(confirm("Apakah anda ingin login?") == true){
+                window.location.href = "https://github.com/trisnaldi"; 
+         } else { alert("Login dibatalkan !");}        
+    } 
+    else{
+        if (done == 0) {
+         alert("Username/Pasword Salah");
+        }
+        }
+} 
